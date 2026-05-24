19c0f7e3b50cc2880abb32dd942964d73381fe5d
Bernard Bosro <bernardbosro@Bernards-MacBook-Pro.local>
2026-05-24 15:56:18 +0000

    change

--- src/app/features/settings/settings-routing.module.ts
+++ src/app/features/settings/settings-routing.module.ts
@@ -2,11 +2,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Settings } from './components/settings/settings';
+import { SubscriptionCallback } from './subscription-callback/subscription-callback';
 
 
 
 const routes: Routes = [
   { path: '', component: Settings },
+  { path: 'subscription/callback', component: SubscriptionCallback },
 
 ];
 
